308d1262afa7ef440343142d8d2bbb5e53f30936
Evgeniy Ivanov <p8635241@gmail.com>
2023-06-09 23:37:46 +0500

    new axios request

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import s from "./Users.module.css"
+import axios from "axios"
 
 export type UsersPropsType = {
     id: number,
@@ -16,41 +17,47 @@ type UsersType = {
     setUsers: (users: UsersPropsType) => void
 }
 export const Users = ({users, follow, unfollow, setUsers}: UsersType): JSX.Element => {
+
     if (users.length === 0) {
-        setUsers([{
-            id: 1,
-            photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmO_Dk5tf21dkiT_2VR3QblcmrWB9dzyV9XQ&usqp=CAU",
-            followed: true,
-            fullName: "Dmitry",
-            status: "I'm a boss",
-            location: {city: "Minsk", country: "Belarus"}
-        },
-            {
-                id: 2,
-                photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmO_Dk5tf21dkiT_2VR3QblcmrWB9dzyV9XQ&usqp=CAU",
-                followed: false,
-                fullName: "Sasha",
-                status: "I'm a boss too",
-                location: {city: "Moscow", country: "Russia"}
-            },
-            {
-                id: 3,
-                photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmO_Dk5tf21dkiT_2VR3QblcmrWB9dzyV9XQ&usqp=CAU",
-                followed: true,
-                fullName: "Valera",
-                status: "No, I'm a boss",
-                location: {city: "Kiev", country: "Ukraine"}
-            },
-            {
-                id: 4,
-                photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmO_Dk5tf21dkiT_2VR3QblcmrWB9dzyV9XQ&usqp=CAU",
-                followed: false,
-                fullName: "Andrew",
-                status: "Nope, I'm a boss",
-                location: {city: "London", country: "UK"}
-            },
-        ])
+        axios.get<any, any>("https://social-network.samuraijs.com/api/1.0").then(response => {
+            debugger
+            setUsers(response)
+        })
+
     }
+    // [{
+    //     id: 1,
+    //     photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmO_Dk5tf21dkiT_2VR3QblcmrWB9dzyV9XQ&usqp=CAU",
+    //     followed: true,
+    //     fullName: "Dmitry",
+    //     status: "I'm a boss",
+    //     location: {city: "Minsk", country: "Belarus"}
+    // },
+    //     {
+    //         id: 2,
+    //         photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmO_Dk5tf21dkiT_2VR3QblcmrWB9dzyV9XQ&usqp=CAU",
+    //         followed: false,
+    //         fullName: "Sasha",
+    //         status: "I'm a boss too",
+    //         location: {city: "Moscow", country: "Russia"}
+    //     },
+    //     {
+    //         id: 3,
+    //         photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmO_Dk5tf21dkiT_2VR3QblcmrWB9dzyV9XQ&usqp=CAU",
+    //         followed: true,
+    //         fullName: "Valera",
+    //         status: "No, I'm a boss",
+    //         location: {city: "Kiev", country: "Ukraine"}
+    //     },
+    //     {
+    //         id: 4,
+    //         photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmO_Dk5tf21dkiT_2VR3QblcmrWB9dzyV9XQ&usqp=CAU",
+    //         followed: false,
+    //         fullName: "Andrew",
+    //         status: "Nope, I'm a boss",
+    //         location: {city: "London", country: "UK"}
+    //     },
+    // ]
 
     return (
         <div>
