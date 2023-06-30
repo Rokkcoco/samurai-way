c970ca3c1ef7f703d3104c88f4000e3123fbaf57
Evgeniy Ivanov <p8635241@gmail.com>
2023-06-30 23:12:15 +0500

    code refactor

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -6,9 +6,20 @@ class ProfileStatus extends React.Component<any> {
     }
 
     activateEditMode() {
-    this.state.editMode = true
-}
-//почему потеряется контекст при декларейшен?
+        this.setState({
+            editMode: true
+        })
+    }
+    deactivateEditMode() {
+        this.setState({
+            editMode: false
+        })
+    }
+
+    //setState асинхронен
+    //force update
+    //почему потеряется контекст при декларейшен?
+    //можно создать через стрелочную функцию и без байнда
     render() {
         return (
             <div>
@@ -17,7 +28,7 @@ class ProfileStatus extends React.Component<any> {
                         <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
                     </div>}
                 {this.state.editMode && <div>
-                    <input value={this.props.status}/>
+                    <input autoFocus onBlur={this.deactivateEditMode.bind(this)} value={this.props.status}/>
                 </div>}
             </div>
         )
