14b613d245f18bfc206815ea74f8f12a0da9b31e
Evgeniy Ivanov <p8635241@gmail.com>
2023-07-05 20:01:52 +0500

    login component form code refactor

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -5,8 +5,7 @@ import dialogsReducer from "./dialogsReducer";
 import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
 import thunk from "redux-thunk";
-
-
+import {reducer as formReducer} from "redux-form";
 
 
 const rootReducer = combineReducers({
@@ -14,7 +13,9 @@ const rootReducer = combineReducers({
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
+
 })
 
 export type AppStateType = ReturnType<typeof rootReducer>
