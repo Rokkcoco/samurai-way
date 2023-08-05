953e00a5009299c486c9626765b284aa34b73d04
Evgeniy Ivanov <p8635241@gmail.com>
2023-08-05 20:42:58 +0500

    captcha for login added

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -3,32 +3,32 @@ import {useForm, UseFormSetError} from "react-hook-form";
 import {yupResolver} from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import {connect} from "react-redux";
-import {login} from "../../redux/authReducer";
+import {getCaptchaUrl, login} from "../../redux/authReducer";
 import {Navigate} from "react-router-dom";
 import {AppRootStateType} from "../../redux/redux-store";
 
 type MapStateToPropsType = {
     isAuth: boolean
+    captchaUrl: any
 }
-const mapPropsToState = (state: AppRootStateType): MapStateToPropsType => ({
-    isAuth: state.auth.isAuth
-})
+
 export type LoginFormTypes = {
     email: string
     password: string
     rememberMe: boolean
 }
-type LoginPropsType = {
-    login: (email: string, password: string, rememberMe: boolean, setError: UseFormSetError<LoginFormTypes>) => void
-    isAuth: boolean
+type LoginPropsType = MapStateToPropsType & {
+    login: (email: string, password: string, rememberMe: boolean,setError: UseFormSetError<LoginFormTypes>, captcha?:string) => void
+    getCaptchaUrl: (captchaUrl: any) => void
 }
-const Login = ({login, isAuth}: LoginPropsType) => {
+const Login = ({login, isAuth, captchaUrl}: LoginPropsType) => {
 
     const loginSchema = yup
         .object({
             email: yup.string().required("This Field cant be empty").max(30, 'Maximum 30 symbols'),
             password: yup.string().required("This Field cant be empty").max(30, 'Maximum 30 symbols').min(6, "Minimum 6 character"),
-            rememberMe: yup.boolean().optional().default(false)
+            rememberMe: yup.boolean().optional().default(false),
+            captcha: yup.string().optional()
         })
         .required()
     const {
@@ -38,8 +38,8 @@ const Login = ({login, isAuth}: LoginPropsType) => {
         setError
     }
         = useForm({resolver: yupResolver(loginSchema)})
-    const onSubmit = (data: { email: string, password: string, rememberMe: boolean }) => {
-        login(data.email, data.password, data.rememberMe, setError)
+    const onSubmit = (data: { email: string, password: string, rememberMe: boolean, captcha: string | undefined}) => {
+        login(data.email, data.password, data.rememberMe, setError, data.captcha)
 
     }
 //   setError('email', {type: "serverSideError", message:"bad"})
@@ -62,7 +62,8 @@ const Login = ({login, isAuth}: LoginPropsType) => {
             </div>
             <div>
                 <input  {...register("rememberMe")} type={"checkbox"}/>remember me
-
+                {captchaUrl && <img src={captchaUrl}/>}
+                {captchaUrl && <input type="text" {...register("captcha")}/>}
             </div>
             <div>
                 <button>Login</button>
@@ -70,6 +71,9 @@ const Login = ({login, isAuth}: LoginPropsType) => {
         </form>
     )
 };
+const mapPropsToState = (state: AppRootStateType): MapStateToPropsType => ({
+    isAuth: state.auth.isAuth,
+    captchaUrl: state.auth.captchaUrl
+})
 
-
-export default connect(mapPropsToState, {login})(Login)
+export default connect(mapPropsToState, {login, getCaptchaUrl})(Login)
--- src/redux/authReducer.ts
+++ src/redux/authReducer.ts
@@ -1,4 +1,4 @@
-import {authAPI} from "../api/api";
+import {authAPI, securityAPI} from "../api/api";
 import {AppThunk} from "./redux-store";
 import {UseFormSetError} from "react-hook-form";
 import {LoginFormTypes} from "../components/Login/Login";
@@ -9,19 +9,22 @@ type InitialStateType = {
     email: string | null
     login: string | null
     isAuth: boolean
+    captchaUrl: any
 }
 //& {isFetching:boolean}
 const initialState = {
     userId: null,
     email: null,
     login: null,
-    isAuth: false
+    isAuth: false,
+    captchaUrl: null
 }
 
 const authReducer = (state: InitialStateType = initialState, action: AuthReducerActionsType): InitialStateType => {
     switch (action.type) {
         case "samurai-way/auth/SET-USER-DATA":
-            return {...state, ...action.payload, isAuth: action.payload.isAuth}
+        case "samurai-way/auth/GET-CAPTCHA-SUCCESS":
+            return {...state, ...action.payload}
         default:
             return state
     }
@@ -30,6 +33,10 @@ const authReducer = (state: InitialStateType = initialState, action: AuthReducer
 export const setAuthUserData = (userId: number | null, email: string | null, login: string | null, isAuth: boolean) => ({
     type: "samurai-way/auth/SET-USER-DATA",
     payload: {userId, email, login, isAuth}
+}) as const
+export const getCaptchaUrlSuccess = (captchaUrl:any) => ({
+    type: "samurai-way/auth/GET-CAPTCHA-SUCCESS",
+    payload: {captchaUrl}
 }) as const
 
 //нужно добавить именно return в начале функции чтобы вернуть наружу результат резолва промиса, но он у нас undefined т.к. ничем не резолвим
@@ -43,13 +50,17 @@ export const getAuthUserData = (): AppThunk => async (dispatch) => {
 
 //можно было добавить return вначале чтобы функция вернула результат промиса наружу
 //так же добавить ошибку или return со значением в самом промисе
-export const login = (email: string, password: string, rememberMe: boolean, setError: UseFormSetError<LoginFormTypes>): AppThunk => {
+export const login = (email: string, password: string, rememberMe: boolean, setError: UseFormSetError<LoginFormTypes>, captcha: string): AppThunk => {
    return async (dispatch) => {
-        const response = await authAPI.login(email, password, rememberMe)
+        const response = await authAPI.login(email, password, rememberMe, captcha)
         console.log(response)
         if (response.data.resultCode === 0) {
             dispatch(getAuthUserData())
-        } else {
+        }
+        else {
+            if (response.data.resultCode === 10) {
+                dispatch(getCaptchaUrl())
+            }
             const message = response.data.messages.length > 0 ? response.data.messages[0] : "Some Error"
             setError("email", {message})
         }
@@ -61,9 +72,16 @@ export const logout = (): AppThunk => async (dispatch) => {
         dispatch(setAuthUserData(null, null, null, false))
     }
 }
+export const getCaptchaUrl = (): AppThunk => async (dispatch) => {
+    const response = await securityAPI.getCaptchaUrl()
+    const captchaURL = response.data.url
+    dispatch(getCaptchaUrlSuccess(captchaURL))
+
+}
 
 
-type AuthReducerActionsType = SetUserDataACType
+type AuthReducerActionsType = SetUserDataACType | GetCaptchaUrlSuccess
 type SetUserDataACType = ReturnType<typeof setAuthUserData>
+type GetCaptchaUrlSuccess = ReturnType<typeof getCaptchaUrlSuccess>
 
 export default authReducer
