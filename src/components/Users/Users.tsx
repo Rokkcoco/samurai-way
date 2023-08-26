7828b0d23f46a91878bc18e039c6b37c83c1b4bb
Evgeniy Ivanov <p8635241@gmail.com>
2023-08-27 00:03:10 +0500

    URI params work properly for users component

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -12,9 +12,13 @@ import {
     getUsersFilter
 } from "../../redux/users-selectors";
 import {useAppDispatch, useAppSelector} from "../../redux/redux-store";
+import {useSearchParams} from "react-router-dom";
 
-
-
+type QueryParamsType = {
+    term?: string
+    friend?: string
+    page?:string
+}
 export const Users = () => {
 
     const totalUsersCount = useAppSelector(getTotalUsersCount)
@@ -24,9 +28,40 @@ export const Users = () => {
     const users = useAppSelector(getUsers)
     const followingInProgress = useAppSelector(getFollowingInProgress)
     const dispatch = useAppDispatch()
+    const [searchParams, setSearchParams] = useSearchParams()
+    const parsed = Object.fromEntries(searchParams)
 
     useEffect(() => {
-        dispatch(requestUsers(currentPage, pageSize, filter))
+       const queryURL:QueryParamsType = {}
+        if (!!filter.term) queryURL.term = filter.term
+        if (filter.friend !== null) queryURL.friend = String(filter.friend)
+        if (currentPage !== 1) queryURL.page = String(currentPage)
+        setSearchParams(queryURL)
+
+    }, [filter, currentPage]);
+
+
+    useEffect(() => {
+
+        let friend: boolean | null = null
+        if (parsed.friend === "true") {
+            friend = true
+        }
+
+        if (parsed.friend === "false") {
+            friend = false
+        }
+
+        const actualPage = +parsed.page || currentPage
+
+        let actualFriend = friend || filter.friend
+        if (friend === false) {
+            actualFriend = friend
+        }
+
+        const actualTerm = parsed.term || filter.term
+        const newFilter = {term: actualTerm, friend: actualFriend}
+        dispatch(requestUsers(actualPage, pageSize, newFilter))
     }, []);
 
     const onPageChanged = (pageNumber: number) => {
--- src/components/Users/UsersSearchForm.tsx
+++ src/components/Users/UsersSearchForm.tsx
@@ -1,25 +1,45 @@
 import {useForm} from "react-hook-form";
-import React, {memo} from "react";
+import React, {memo, useEffect} from "react";
 import {FilterType} from "../../redux/users-reducer";
+import {useAppSelector} from "../../redux/redux-store";
+import {getUsersFilter} from "../../redux/users-selectors";
+
 type Props = {
     onFilterChanged: (filter: FilterType) => void
 }
+type FriendFormType = "null" | "true" | "false";
 type FormType = {
     term: string
-    friend: "null" | "true" | "false"
+    friend: FriendFormType
 }
-export const UsersSearchForm = memo(({onFilterChanged}:Props) => {
-    const {register, handleSubmit} = useForm<FormType>({defaultValues: {term: "", friend: "null"}})
+export const UsersSearchForm = memo(({onFilterChanged}: Props) => {
+    const filter = useAppSelector(getUsersFilter)
+    const {register, handleSubmit, setValue} = useForm<FormType>({
+        defaultValues: {
+            term: filter.term,
+            friend: String(filter.friend) as FriendFormType
+        }
+    })
     const onSubmit = (data: FormType) => {
-        onFilterChanged({term: data.term, friend: data.friend === "null" ? null : data.friend === "true"})
+        onFilterChanged({
+            term: data.term,
+            friend: data.friend === "null" ? null : data.friend === "true"
+        })
     }
+    useEffect(() => {
+
+        setValue("term", filter.term)
+        setValue("friend", String(filter.friend) as FriendFormType)
+    }, [filter]);
+    console.log(filter)
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <div>
                 <input type="text" {...register("term")}/>
                 <select {...register("friend")}>
-                    <option value="false">All users</option>
+                    <option value="null">All users</option>
                     <option value="true">Only followed</option>
+                    <option value="false">Only unfollowed</option>
                 </select>
                 <button>Search</button>
             </div>
